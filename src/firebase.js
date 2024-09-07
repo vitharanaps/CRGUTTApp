d05bf0364cb864390b8738e758825392279cc219
vitharanaps <120036474+vitharanaps@users.noreply.github.com>
2024-09-07 09:05:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyAMy4XTsYNgffUOT-P2JsOqN3ZjvD6nqWw",
+
   authDomain: "crgu-tt-app-1.firebaseapp.com",
   projectId: "crgu-tt-app-1",
   storageBucket: "crgu-tt-app-1.appspot.com",
